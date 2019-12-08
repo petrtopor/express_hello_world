6f778c12a80ab4371193239280bbffac731c6c6f
petrtopor <coder.ru@gmail.com>
2019-12-09 01:44:06 +0400

    added cors library

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-
-  app.options('*', (req, res) => {
-      // allowed XHR methods  
-      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-      res.send();
-  });
-});
+app.use(cors())
 
 app.get('/', (req, res) => res.send(`Hello World from Vagiz's express test server!`))
 
