d77ab98e01b865d918706a299b198500e467bbfc
petrtopor <coder.ru@gmail.com>
2019-12-09 01:11:03 +0400

    CORS issue fixed

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      // allowed XHR methods  
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
+});
+
 app.get('/', (req, res) => res.send(`Hello World from Vagiz's express test server!`))
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
