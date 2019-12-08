ba811bca9b82f61ad0b35f1b34f87b706594f130
petrtopor <coder.ru@gmail.com>
2019-12-08 15:33:26 +0400

    Displaying port number is now 8080

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World from GCE!'))
+app.get('/', (req, res) => res.send(`Hello World from Vagiz's express test server!`))
 
-app.listen(8080, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
